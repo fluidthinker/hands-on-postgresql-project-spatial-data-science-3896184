5862420cc0b483478c0bb78e0e44d5fbb563dd4f
fluidthinker <74799212+fluidthinker@users.noreply.github.com>
2026-02-20 03:13:38 +0000

    fixed the alter table command

--- 04_01/04_01_solution.sql
+++ 04_01/04_01_solution.sql
@@ -2,6 +2,11 @@
 -- hint 2: working with projection ST_Transform() and ST_SetSRID - https://postgis.net/documentation/tips/st-set-or-transform/
 
 -- Goal: transform census tract boundary files with UTM 18N projection
+-- alter table nyct2020
+-- alter column wkb_geometry type geometry(MultiPolygon, 32618)
+-- using ST_Transform(ST_SetSRID(wkb_geometry,4326), 32618);
+
+
 alter table nyct2020
 alter column wkb_geometry type geometry(MultiPolygon, 32618)
-using ST_Transform(ST_SetSRID(wkb_geometry,4326), 32618);
+using ST_Transform(wkb_geometry::geometry, 32618);
